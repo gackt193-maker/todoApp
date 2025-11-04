d4b5f4e009a59b0d5ae33ca503cb39ceb31423ab
Shinji <gackt193@gmail.com>
2025-11-04 17:27:56 +0900

    関数を作成し、jsの問題点とtsのメリットを確認した

--- survivalTS/increment.ts
+++ survivalTS/increment.ts
@@ -1,8 +1,11 @@
-function increment(num) {
-    //                 ^^^^^^^^^^型注釈
+
+function increment(num: number) {
+//                 ^^^^^^^^^^型注釈
     return num + 1;
 }
+
 console.log(increment(999));
+
 /*
 console.log(increment("999")); // tsc increment.tsコマンドでチェック
 => エラーが発生する
@@ -11,4 +14,4 @@ Argument of type 'string' is not assignable to parameter of type 'number'.
 9 console.log(increment("999"));
                         ~~~~~
 Found 1 error in increment.ts:9
-*/ 
+*/
